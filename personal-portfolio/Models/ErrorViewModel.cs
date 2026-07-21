dbe1852a90c29f7a8fa4492e1db5510615b657ac
Meltem Ergül <meltemergul34@gmail.com>
2026-07-21 17:01:24 +0300

    first commit

---
+++ personal-portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace personal_portfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
